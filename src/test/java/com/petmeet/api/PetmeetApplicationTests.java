c46013038910733f748846ff4082f8b59ed4b899
songhyeonpk <ehftozl1997@gmail.com>
2024-08-10 16:48:51 +0900

    프로젝트 초기 셋팅

---
+++ src/test/java/com/petmeet/api/PetmeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petmeet.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetmeetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
