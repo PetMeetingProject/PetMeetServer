c1e1246a9d67d1ca089deae04af5547869ada1fe
songhyeonpk <ehftozl1997@gmail.com>
2024-08-10 16:48:51 +0900

    SET: 프로젝트 초기 셋팅

---
+++ src/main/java/com/petmeet/api/PetmeetApplication.java
@@ -0,0 +1,13 @@
+package com.petmeet.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetmeetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetmeetApplication.class, args);
+    }
+
+}
