002f7f9d9540043207b1bb258e4b17063aceab2d
songhyeonpk <ehftozl1997@gmail.com>
2024-09-26 14:02:40 +0900

    FEAT: 공통 API 응답 클래스 추가

---
+++ src/main/java/com/petmeet/api/api/controller/dto/response/BaseApiResponse.java
@@ -0,0 +1,35 @@
+package com.petmeet.api.api.controller.dto.response;
+
+import com.petmeet.api.core.enums.SuccessResponseType;
+import com.petmeet.api.core.exception.errorcode.BaseErrorCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class BaseApiResponse<D> {
+
+    private final HttpStatus httpStatus;
+    private final String message;
+    private final D data;
+
+    private BaseApiResponse(HttpStatus httpStatus, String message, D data) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+        this.data = data;
+    }
+
+    // 성공 응답 (응답 데이터가 있는 경우)
+    public static <D> BaseApiResponse<D> success(SuccessResponseType responseType, D data) {
+        return new BaseApiResponse<>(responseType.getHttpStatus(), responseType.getMessage(), data);
+    }
+
+    // 성공 응답 (응답 데이터가 없는 경우)
+    public static <D> BaseApiResponse<D> success(SuccessResponseType responseType) {
+        return new BaseApiResponse<>(responseType.getHttpStatus(), responseType.getMessage(), null);
+    }
+
+    // 실패, 에러 응답
+    public static <D> BaseApiResponse<D> error(BaseErrorCode errorCode) {
+        return new BaseApiResponse<>(errorCode.getHttpStatus(), errorCode.getMessage(), null);
+    }
+}
